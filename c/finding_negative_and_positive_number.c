c558e88b1347bb3998835e753189477e71ea9fb2
Tafhimul kabir <tafhimulkabir@gmail.com>
2022-12-07 22:28:33 +0600

    find negative and positive number in c

---
+++ c/finding_negative_and_positive_number.c
@@ -0,0 +1,37 @@
+/**
+ * @file finding_negative_and_positive_number.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-07
+ * 
+ * @copyright Copyright (c) 2022
+ *
+ * C program to find negative and positive number
+ */
+
+// Include standard input output library
+#include <stdio.h>
+
+int main()
+{
+    // Declaring variables
+    int Num;
+    printf("Enter Number: ");
+    scanf("%d", &Num);
+    
+    //A negative number is less than 0, 
+    //So the if suggests that if the program detects any number less than 0, 
+    //then it must be a negative    
+    if(Num < 0)
+    {
+        printf("%d is a Negative number", Num);
+    }
+    //Otherwise, the program will consider the number as positive.
+    else
+    {
+        printf("%d is a Positive number", Num);
+    }
+
+    return 0;
+}
